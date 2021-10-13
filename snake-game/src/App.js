fac6983f91040ac43560903064fa44048b3ddcf2
fayeduoduo <fayeduoduo@gmail.com>
2021-10-12 21:26:17 -0700

    Add files via upload

---
+++ snake-game/src/App.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react'
+import Food from './Components/Food';
+import Snake from './Components/Snake';
+
+
+const getRandomCoordinators = () => {
+  let min = 1;
+  let max = 98;
+  let x = Math.floor((Math.random() * max + min) / 2) * 2
+  let y = Math.floor((Math.random() * max + min) / 2) * 2
+  return [x,y]
+}
+
+
+const initailState = {
+    food: getRandomCoordinators(),
+    direction: 'RIGHT',
+    speed: 200,
+    snakeDots: [
+    // [left,top]
+      [0,0],[2,0]
+    ]
+}
+class App extends Component {
+  state = initailState
+
+  componentDidMount() {
+    document.onkeydown = this.onkeydown;
+    setInterval(this.moveSnake, this.state.speed)
+  }
+
+  //更新状态，就调用判断边界的函数
+  componentDidUpdate() {
+    this.checkIfOutBorders();
+    this.checkIfCollapsed();
+    this.checkIfEat();
+  }
+  //keyboard function
+  onkeydown = (e) => {
+    switch (e.keyCode) {
+      
+      case 38:
+        this.setState({ direction: 'UP' });
+        break;
+      case 40:
+        this.setState({ direction: 'DOWN' });
+        break;
+      case 37:
+        this.setState({ direction: 'LEFT' });
+        break;
+      case 39:
+        this.setState({ direction: 'RIGHT' });
+        break;
+      default:
+        this.setState({ direction: 'RIGHT' });
+    }
+  }
+
+  //move snake function
+  moveSnake = () => {
+    let dots = [...this.state.snakeDots]
+    // head equal array tail
+    let head = dots[dots.length - 1]
+    
+    switch (this.state.direction) {
+      case 'RIGHT':
+        head = [head[0] + 2, head[1]]
+        break;
+      case 'LEFT':
+        head = [head[0] - 2, head[1]]
+        break;
+      case 'UP':
+        head = [head[0], head[1] - 2]
+        break;
+      case 'DOWN':
+        head = [head[0], head[1] +2]
+        break;
+      default:
+        break;
+    }
+    //add ele to the last place
+    dots.push(head);
+    //delete the first ele of array
+    dots.shift();
+    this.setState({
+      snakeDots: dots
+    })
+  }
+  
+  checkIfOutBorders() {
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+    // console.log(head)
+    if (head[0] >= 100 || head[0] < 0 || head[1] >= 100 || head[1] < 0) {
+      this.onGameOver();
+    }
+  }
+
+  checkIfCollapsed() {
+    let snake= [...this.state.snakeDots]
+    let head = snake[snake.length - 1]
+    //delete the last ele of array in case collide 
+    snake.pop();
+    snake.forEach(dot => {
+      if (head[0] === dot[0] && head[1] === dot[1]) {
+        this.onGameOver()
+      }
+    })
+  }
+
+  checkIfEat() {
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+    let food = this.state.food;
+
+    if (head[0] === food[0] && head[1] === food[1]) {
+      this.setState({
+        food: getRandomCoordinators()
+      })
+      this.enlargeSnake();
+      this.increaseSpeed()
+    }
+  }
+  
+  increaseSpeed() {
+    if (this.state.speed > 10) {
+      this.setState({speed: this.state.speed - 10})
+    }
+  }
+
+  enlargeSnake() {
+    let newSnake = [...this.state.snakeDots];
+    // add one ele at first place of array
+    newSnake.unshift([])
+    this.setState({ snakeDots: newSnake })
+  }
+
+  onGameOver(){
+    alert(`game over, you score is ${this.state.snakeDots.length - 2}`);
+    //refresh game page
+    this.setState(initailState)
+  }
+  render() {
+    return (
+      <div className='game-area'>
+        <Snake snakeDots={this.state.snakeDots} />
+        <Food food={this.state.food}/>
+      </div>
+    )
+  }
+}
+
+export default App;
